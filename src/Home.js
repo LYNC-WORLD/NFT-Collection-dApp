d9c410ba7ac8f3d845bc8a9517dee4d0429d38b2
Tanvi <tanvitomar592@gmail.com>
2023-04-18 15:37:56 +0530

    Made Responsive

--- src/Home.js
+++ src/Home.js
@@ -1,40 +1,40 @@
 import lyncIcon from "./assets/lync-icon.svg";
 
 const HomePage = () => {
-    return (
-        <>
-            <div className="homepage">
-                <main className="homepage-content">
-                    <span className="lync-icon">
-                        <img src={lyncIcon} alt="lync_icon" />
-                    </span>
-                    <h1 className="homepage-heading">
-                        Building Your Claimer, We Simplify This For You! <br />{" "}
-                        Use This Boilerplate and Get Going... !
-                    </h1>
-                    <p className="homepage-description">
-                        Don&apos;t forget to explore the world of{" "}
-                        <a
-                            href="https://lync.world/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            LYNC
-                        </a>{" "}
-                        though !!
-                    </p>
-                    <a
-                        href="https://betanft.lync.world/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="nft-deployer-link"
-                    >
-                        Try Deploying Your NFT
-                    </a>
-                </main>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="homepage">
+        <main className="homepage-content">
+          <span className="lync-icon">
+            <img src={lyncIcon} alt="lync_icon" />
+          </span>
+          <h1 className="homepage-heading">
+            Building Your Claimer, We Simplify This For You! Use This
+            Boilerplate and Get Going... !
+          </h1>
+          <p className="homepage-description">
+            Don&apos;t forget to explore the world of{" "}
+            <a
+              href="https://lync.world/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LYNC
+            </a>{" "}
+            though !!
+          </p>
+          <a
+            href="https://betanft.lync.world/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="nft-deployer-link"
+          >
+            Try Deploying Your NFT
+          </a>
+        </main>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
